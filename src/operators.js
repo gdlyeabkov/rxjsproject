afa38154059953a83637df4eabb4d8c70cb8702e
Rodion <diakovrodion@mail.ru>
2021-05-17 20:54:09 +0300

    fixed bug

--- src/operators.js
+++ src/operators.js
@@ -1,37 +1,33 @@
-import {,map,take,takeLast,takeWhile,scan,reduce,filter,tap} from 'rxjs/operators'
-import {fromEvent,interval} from 'rxjs'
+import { map, take, takeLast, takeWhile, scan, reduce, filter, tap } from 'rxjs/operators'
+import { fromEvent, interval } from 'rxjs'
 fromEvent(document,'click').pipe(
     switchMap((event)=>{
         return interval(1000).pipe(
-            tap(v=>console.log(v))
+            tap(v=>console.log(v)),
             stream$.subscribe({
                 next:v=>console.log(v),
                 complete:()=>console.log("complete")
-                
             })
         )
     })
 ).subscribe(()=>{
     const stream$=interval(1000).pipe(
-        tap(v=>console.log(v))
+        tap(v=>console.log(v)),
         stream$.subscribe({
             next:v=>console.log(v),
             complete:()=>console.log("complete")
             
         })
     )
-const stream$=interval(1000).pipe(
-    tap(v=>console.log(v)),
-    map((v)=>v*2),
-    filter(v=>v %2 ===0),
-    take(10),
-    reduce((acc,v)=>acc+v,[])
-    // scan((acc,v)=>acc+v,[])
-    // takeWhile(v=>v < 7)
-    // takeLast(5)
-)
-stream$.subscribe({
-    next:v=>console.log(v),
-    complete:()=>console.log("complete")
-    
+    const stream$=interval(1000).pipe(
+        tap(v=>console.log(v)),
+        map((v)=>v*2),
+        filter(v=>v %2 ===0),
+        take(10),
+        reduce((acc,v)=>acc+v,[])
+    )
+    stream$.subscribe({
+        next:v=>console.log(v),
+        complete:()=>console.log("complete") 
+    })
 })
--- src/subjects.js
+++ src/subjects.js
@@ -1,15 +1,5 @@
 import {Subject,BehaviorSubject,ReplaySubject} from 'rxjs'
 document.addEventListener('click',()=>{
-    // const stream$=new Subject()
-    // stream$.subscribe(v=>console.log(v))
-    // stream$.next()
-
-    
-    // const stream$=new BehaviorSubject('First')
-    // stream$.subscribe(v=>console.log(v))
-    // stream$.next()
-
-    
     const stream$=new ReplaySubject(1)
     stream$.subscribe(v=>console.log(v))
     stream$.next()
